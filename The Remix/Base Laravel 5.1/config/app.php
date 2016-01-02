db8432e216fc04effe302c5950e88378a197bece
michimawan <openpublick@gmail.com>
2016-01-02 09:29:23 +0700

    adding service provider and aliases package

--- The Remix/Base Laravel 5.1/config/app.php
+++ The Remix/Base Laravel 5.1/config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -196,6 +197,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Html'      => Illuminate\Html\HtmlFacade::class,
         'Form'      => Illuminate\Html\FormFacade::class,
+        'Image'     => Intervention\Image\Facades\Image::class,
 
     ],
 
